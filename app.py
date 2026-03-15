8c57f6d04c86ad5869fafca0bf0a1fa62ff42953
Marcio1995 <123603516+marcionoronha1995@users.noreply.github.com>
2026-03-15 08:19:59 -0300

    .venv app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Rota Principal - É aqui que o Flask lê o seu index.html
+@app.route('/')
+def index():
+    # O Flask procura automaticamente na pasta /templates
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Roda o servidor localmente para teste
+    app.run(debug=True)
